d13f8d8defd89f2cda348f5a2e7c97cde5df3dee
Viktor Drykin <viktordrykin@Viktors-MacBook-Pro.local>
2025-05-06 07:46:23 +0200

    init project

---
+++ CarMarket/CarMarket/CarMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarMarketApp.swift
+//  CarMarket
+//
+//  Created by Viktor Drykin on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CarMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarMarket/CarMarket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarMarket
+//
+//  Created by Viktor Drykin on 06.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
